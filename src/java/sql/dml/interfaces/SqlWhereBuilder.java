6f9407e709a10530bd4faabfc5ff5c7b2b6e4996
padmelina <elizaveta.sukhanova@rt.ru>
2020-05-07 22:40:41 +0300

    add where exists with tests

--- src/java/sql/dml/interfaces/SqlWhereBuilder.java
+++ src/java/sql/dml/interfaces/SqlWhereBuilder.java
@@ -5,4 +5,5 @@ import sql.operators.ComparisonOperators;
 public interface SqlWhereBuilder extends SqlFinalBuilder {
     SqlFinalBuilder where(String clause);
     SqlFinalBuilder where(String lValue, ComparisonOperators operator, String rValue);
+    SqlFinalBuilder whereExists(SqlFinalBuilder query);
 }
